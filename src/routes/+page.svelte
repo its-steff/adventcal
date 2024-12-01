7da68182adcba20582fdb099725046f37e27e917
its-steff <steffdziadkowiec@proton.me>
2024-12-01 22:08:16 +0100

    first commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,14 @@
+<script>
+	import adventfox from '$lib/images/adventfox.jpg';
+</script>
+
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+
+<img src={adventfox} alt="drawing of a fox" />
+
+<style>
+	img {
+		width: 200px;
+	}
+</style>
